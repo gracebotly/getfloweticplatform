22f615a91ae786141c1726f68fabd895fc6a2db2
Agentflow <gracebolachannel@gmail.com>
2026-03-12 09:19:47 -0400

    fix: disable no-empty-object-type ESLint rule for shadcn component compatibility

--- eslint.config.js
+++ eslint.config.js
@@ -18,6 +18,7 @@ export default tseslint.config(
     rules: {
       ...reactHooks.configs.recommended.rules,
       "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-empty-object-type": "off",
     },
   },
 );
